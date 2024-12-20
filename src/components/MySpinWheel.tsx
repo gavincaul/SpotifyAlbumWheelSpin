a675d05c349020acce409b5ab2e6907728715c5f
gavincaul <gavcaul@udel.edu>
2024-12-20 12:17:21 -0500

    moves api

--- src/components/MySpinWheel.tsx
+++ src/components/MySpinWheel.tsx
@@ -8,7 +8,7 @@ const MySpinWheel = (code) => {
     useEffect(() => {
       const fetchAlbums = async () => {
         try {
-          const response = await fetch(`/api/spotify/?code=${code.code}`);
+          const response = await fetch(`/SpotifyAlbumWheelSpin/api/spotify/?code=${code.code}`);
           if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
           }
