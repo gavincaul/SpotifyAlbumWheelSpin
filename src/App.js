01ec5682a99607618234c496e1efa91c044bb4b7
gavin <gavcaul@udel.edu>
2026-01-11 15:56:49 -0500

    songdataquote

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import MySpinWheel from "./components/MySpinWheel.tsx";
-import AuthButton from "./components/AuthButton.tsx"; 
+import AuthButton from "./components/AuthButton.tsx";
 
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -10,7 +10,7 @@ export default function App() {
     const queryParams = new URLSearchParams(window.location.search);
     const code = queryParams.get("code");
     if (code) {
-      setCodeValue(code)
+      setCodeValue(code);
       setIsAuthenticated(true); // If there's a token, the user is authenticated
     }
   }, []);
@@ -18,10 +18,10 @@ export default function App() {
   return (
     <div className="App">
       <h1>Spotify Album Wheel Spinner</h1>
-      
+
       {isAuthenticated ? (
         // Show the SpinWheel only after authentication
-        <MySpinWheel code = {codeValue}/>
+        <MySpinWheel code={codeValue} />
       ) : (
         // Show the login button if not authenticated
         <div>
@@ -29,7 +29,14 @@ export default function App() {
           <AuthButton /> {/* Button that triggers the OAuth flow */}
         </div>
       )}
-      
+      <footer>
+        <p>
+          Music data provided by{" "}
+          <a href="https://getsongbpm.com" target="_blank" rel="noreferrer">
+            GetSongBPM
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
